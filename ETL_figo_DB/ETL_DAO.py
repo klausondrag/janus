1f5e51f9c26dae0ffebd95095ffcae9962dd0903
ga26seb <quang.thien@gmx.de>
2017-06-24 15:05:40 +0200

    updates dummy data and implements DAO methods

--- ETL_figo_DB/ETL_DAO.py
+++ ETL_figo_DB/ETL_DAO.py
@@ -1,26 +1,59 @@
 #!/usr/bin/env python
 
-def main():
-    print("a")
-    a = ETL_DAO
-    a.id = 1
-    a.load_contacts()
+import json
+from typing import List
+import Contact
+
+class Contact:
+    name = ''
+    iban = ''
+    bic = ''
+
+    def __init__(self, name: str, iban: str, bic: str) -> None:
+        super().__init__()
+        self.name = name
+        self.iban = iban
+        self.bic = bic
 
 class ETL_DAO:
-    id = 0
+    id = ""
 
-    def load_contact(iban: str, bic: str, id: int):
+    def __init__(self, id: str) -> None:
+        self.id = id
+
+# Private methods
+    def json_to_Contact(self, jdata) -> Contact:
+        return Contact(jdata["name"], jdata["iban"], jdata["bic"])
+
+# Interface
+
+    def load_contact(self, iban: str, bic: str, id: str):
         pass
 
-    def load_contacts():
-        with open("dummmy_data.json", "r") as data_file:
+    def load_contacts(self) -> List[Contact]:
+        with open("dummy_data.json", "r") as data_file:
             data = json.loads(data_file.read())
             users = data["users"]
+            contacts_json = []
             for u in users:
-                print(u)
+                if u["id"] == self.id:
+                    contacts_json = u["contacts"]
 
-    def save_contact(iban: str, bic: str):
+            contacts = []
+            for c in contacts_json:
+                contacts.append(self.json_to_Contact(c))
+
+            return contacts
+
+    def save_contact(self, iban: str, bic: str):
         pass
 
-    def save_contacts(contactlist):
+    def save_contacts(self, contacts: List[Contact]):
         pass
+
+def main():
+        print("a")
+        a = ETL_DAO("1")
+        a.load_contacts()
+
+main()
