1f187004b7b8149043b2b86ff0ae9e34fe9b98f5
OndragKlaus <klaus.ondrag@outlook.com>
2017-06-24 14:29:54 +0200

    implemented search_contacts and send_money with mock data

--- middleware/mock.py
+++ middleware/mock.py
@@ -9,6 +9,9 @@ class Contact:
         self.iban = iban
         self.bic = bic
 
+    def __str__(self):
+        return str(vars(self))
+
 
 class User:
     def __init__(self, id: str) -> None:
@@ -21,3 +24,9 @@ class User:
         else:
             return [Contact('Niklas', 'DE21 0000', ''),
                     Contact('Thien', 'DE23 0000', '')]
+
+
+class FigoConnector:
+    @staticmethod
+    def send_money(contact: Contact, money_amount: int, currency: str) -> bool:
+        pass
--- middleware/send_money.py
+++ middleware/send_money.py
@@ -1,9 +1,16 @@
 from typing import List
 
-from middleware.interface import Contact
-from mock import User
+from middleware.interface import Contact as MWContact
+from mock import User, Contact as BEContact, FigoConnector
+import transformer as t
 
 
-def search_contacts(user_id: str, name: str) -> List[Contact]:
+def search_contacts(user_id: str, name: str) -> List[BEContact]:
     user = User(user_id)
-    return [c for c in user.get_contacts() if name.lower() in c.name.lower()]
+    matches = [c for c in user.get_contacts() if name.lower() in c.name.lower()]
+    matches = [t.BEContact_to_MWContact(c) for c in matches]
+    return matches
+
+
+def send_money(contact: MWContact, money_amount: int, currency: str) -> bool:
+    return FigoConnector.send_money(t.MWContact_to_BEContact(contact), money_amount, currency)
