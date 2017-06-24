1e412e0012e68e23f2864c33f0ec6321bdc2654b
OndragKlaus <klaus.ondrag@outlook.com>
2017-06-24 18:19:04 +0200

    implemented loading from and saving to json

--- ETL_figo_DB/dao.py
+++ ETL_figo_DB/dao.py
@@ -1,16 +1,97 @@
 #!/usr/bin/env python
 
 import json
-from typing import List
-from contact import Contact
-from transaction import Transaction
+from typing import List, Dict, Collection, Mapping
+
+
+class Contact:
+    def __init__(self, name, iban, bic):
+        self.name = name
+        self.iban = iban
+        self.bic = bic
+
+    def to_json(self):
+        return {
+            'name': to_json(self.name),
+            'iban': to_json(self.iban),
+            'bic': to_json(self.bic)
+        }
+
+
+class Transaction:
+    def __init__(self, id, contact, type, purpose, booking_text, amount):
+        self.id = id
+        self.contact = Contact(**contact)
+        self.type = type
+        self.purpose = purpose
+        self.booking_text = booking_text
+        self.amount = amount
+
+    def to_json(self):
+        return {
+            'id': to_json(self.id),
+            'contact': to_json(self.contact),
+            'type': to_json(self.type),
+            'purpose': to_json(self.purpose),
+            'booking_text': to_json(self.booking_text),
+            'amount': to_json(self.amount)
+        }
+
+
+class User:
+    def __init__(self, contacts: List[Contact], transaction: List[Transaction]):
+        self.contacts = contacts
+        self.transactions = transaction
+
+    def to_json(self):
+        return {
+            'contacts': to_json(self.contacts),
+            'transactions': to_json(self.transactions)
+        }
+
+
+class Data:
+    def __init__(self, users: Dict[str, User]):
+        self.users = users
+
+    def to_json(self):
+        return {
+            "users": to_json(self.users)
+        }
+
+
+def to_json(obj):
+    if hasattr(obj, 'to_json'):
+        return obj.to_json()
+    elif isinstance(obj, str):
+        return obj
+    elif isinstance(obj, Mapping):
+        return {to_json(k): to_json(v) for k, v in obj.items()}
+    elif isinstance(obj, Collection):
+        return [to_json(x) for x in obj]
+    else:
+        return obj
 
 
 class DAO:
-    def __init__(self, id: str) -> None:
-        self.id = id
+    def __init__(self, user_id: str) -> None:
+        self._file_name = 'dummy_data.json'
+        self.user_id = user_id
+        self._data = self._load_data()
 
-    ### Interface methods ###
+    def _load_data(self) -> Data:
+        with open(self._file_name, 'r') as data_file:
+            json_text = json.loads(data_file.read())
+            users = {}
+            for k, v in json_text['users'].items():
+                contacts = [Contact(**c) for c in v['contacts']]
+                transactions = [Transaction(**t) for t in v['transactions']]
+                users[k] = User(contacts, transactions)
+            return Data(users)
+
+    def _save_data(self) -> None:
+        with open(self._file_name, 'w') as data_file:
+            json.dump(to_json(self._data), data_file, indent=2)
 
     def load_contacts(self) -> List[Contact]:
         with open("dummy_data.json", "r") as data_file:
@@ -58,3 +139,8 @@ class DAO:
 
     def write_contacts(self, contacts: List[Contact]) -> None:
         pass
+
+
+if __name__ == '__main__':
+    d = DAO('1')
+    d._save_data()
--- ETL_figo_DB/test_dao.py
+++ ETL_figo_DB/test_dao.py
@@ -1,8 +1,9 @@
 from contact import Contact
 from dao import DAO
 
+
 def main():
     d = DAO("1")
-    d.save_contact(Contact("BLA", "AL47212110090000000235698741", "OKOYFIHH"))
+
 
 main()
