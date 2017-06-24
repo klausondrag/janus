3890517baf4290c4053d2cc616c3cab779c3e446
vauhochzett <vauhochzett@users.noreply.github.com>
2017-06-24 20:46:41 +0200

    Adds poll_error

--- middleware/api.py
+++ middleware/api.py
@@ -3,6 +3,8 @@ from typing import List
 from .interface import Contact
 from .mock import User, Contact as BEContact, FigoConnector
 from . import transformer as t
+from ETL_figo_DB.dao import DAO
+from ETL_figo_DB.etl import ETL
 
 
 def search_contacts(user_id: str, name: str) -> List[BEContact]:
@@ -18,3 +20,17 @@ def send_money(contact: Contact, money_amount: int, currency: str) -> bool:
 
 def update_figo(add_demo_data: bool) -> None:
     FigoConnector.update(add_demo_data)
+
+
+def poll_errors(user_id: str, throw_error: bool) -> List[Transaction]:
+    etl.update(throw_error)
+    dao = DAO(user_id)
+    transactions = dao.load_transactions()
+    for t in transactions:
+        if t.contact.iban == "GE29NB0000000101904900":
+            return [t]
+    
+    return []
+
+if __name__ == '__main__':
+    poll("A1.1", False)
