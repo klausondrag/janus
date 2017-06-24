3218e9990cd7fc8983bc1166c9eb22ebca50b983
vauhochzett <vauhochzett@users.noreply.github.com>
2017-06-24 17:27:35 +0200

    Implements first stubs and comments

--- ETL_figo_DB/etl.py
+++ ETL_figo_DB/etl.py
@@ -4,4 +4,16 @@ from typing import List
 
 class ETL:
     def update(add_demo_data: bool) -> None:
+        transactions = get_formatted_transactions()
+        # 3. Transform data
+        #    a. Extract contacts and transform to Contact
+        #    b. Transform transactions
+        # 4. Save contacts, transactions and meta (last access)
+        pass
+
+
+    ### Private methods ###
+    def get_formatted_transactions() -> List[Transaction]:
+        # 1. get_accounts
+        # 2. get_transactions
         pass
